09052a72403b4f604fc7b3f8b69122d56fc4d704
handriss <andrashinkel@gmail.com>
2017-02-20 14:50:50 +0100

    sender working

--- tk-02-tw-thermometer/main.cpp
+++ tk-02-tw-thermometer/main.cpp
@@ -1,15 +1,38 @@
 #include "mbed.h"
-#include "codecool/codecool_shield_names.h"
+#include "codecool_serial.h"
+#include "codecool_joystick.h"
+#include "codecool_lcd.h"
 
 int main() {
-	gpio_t led;
+    SERIAL_BAUD(9600);
+    SERIAL_SET_NON_BLOCKING();
 
-	gpio_init_out(&led, LED_GREEN_SHIELD);
+    uint8_t buffer[64];
+    memset(buffer, 0x00, sizeof(buffer));
 
-	while(1) {
-		gpio_write(&led, 1);
-		wait(0.5);
-		gpio_write(&led, 0);
-		wait(0.5);
-	}
+    LCD_CLS();
+    LCD_LOCATE(0, 0);
+    LCD_PRINTF("Starting...");
+
+    while (1) {
+    	if(JOYSTICK_PUSHED == 1){
+            strncpy((char*)buffer, "Hello there!", 12);
+            LCD_PRINTF("Sending data...");
+            LCD_CLS();
+            LCD_LOCATE(0, 0);
+            SERIAL_SEND(buffer, strlen((char*)buffer));
+            LCD_PRINTF("Data sent: %s", buffer);
+            wait(1);
+
+    	}
+
+
+//        while(SERIAL_AVAILABLE() < 12 );
+//
+//        memset(buffer, 0x00, sizeof(buffer));
+//        SERIAL_RECV(buffer, 12);
+//        D("Received: %s", buffer);
+//
+//        wait(5);
+    }
 }
