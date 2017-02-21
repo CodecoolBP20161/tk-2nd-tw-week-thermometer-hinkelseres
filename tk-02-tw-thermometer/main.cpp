716692545f9f3da0586227c55fc39ececf442292
handriss <andrashinkel@gmail.com>
2017-02-21 10:48:51 +0100

    sending working

--- tk-02-tw-thermometer/main.cpp
+++ tk-02-tw-thermometer/main.cpp
@@ -2,37 +2,54 @@
 #include "codecool_serial.h"
 #include "codecool_joystick.h"
 #include "codecool_lcd.h"
+#include "codecool_i2c.h"
+
+#define LM75_ADDRESS 0x90
+
 
 int main() {
     SERIAL_BAUD(9600);
     SERIAL_SET_NON_BLOCKING();
 
-    uint8_t buffer[64];
+    I2C_FREQ(100000);
+
+    uint8_t buffer[12];
     memset(buffer, 0x00, sizeof(buffer));
+
+    float temp;
 
     LCD_CLS();
     LCD_LOCATE(0, 0);
     LCD_PRINTF("Starting...");
 
     while (1) {
+
+
+    	buffer[0] = 0x00;
+		I2C_WRITE(LM75_ADDRESS, buffer, 1);
+
+		memset(buffer, 0x00, sizeof (buffer));
+		I2C_READ(LM75_ADDRESS, buffer, 2);
+
+
+		int8_t _int_part = (int8_t)buffer[0];
+		temp = _int_part + 0.5f * ((buffer[1]&0x80)>>7);
+
+		LCD_CLS();
+		LCD_LOCATE(0, 0);
+		LCD_PRINTF("Temperature: %0.1f\n", temp);
+
+		wait(0.2);
+
     	if(JOYSTICK_PUSHED == 1){
-            strncpy((char*)buffer, "Hello there!", 12);
+
             LCD_PRINTF("Sending data...");
             LCD_CLS();
             LCD_LOCATE(0, 0);
-            SERIAL_SEND(buffer, strlen((char*)buffer));
+            SERIAL_SEND(buffer, 12);
             LCD_PRINTF("Data sent: %s", buffer);
             wait(1);
 
     	}
-
-
-//        while(SERIAL_AVAILABLE() < 12 );
-//
-//        memset(buffer, 0x00, sizeof(buffer));
-//        SERIAL_RECV(buffer, 12);
-//        D("Received: %s", buffer);
-//
-//        wait(5);
     }
 }
